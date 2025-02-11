c26540a291da8bfa6735a07c8b1553ea1c806f84
koiiiiiiiiii <115970207+koiiiiiiiiii@users.noreply.github.com>
2025-02-11 21:12:12 +0300

    Update findSmallest.py
    /1、不打印元组了，只打印最小值索引，并且使用最小排序算法对给定列表进行排序，排序前必须使用索引找到对应位置后才能进行重新添加列表值，后输出排序后的列表
    2、解决了return必须在定义函数内部的问题

--- 01_introduction_to_algorithms/python/findSmallest.py
+++ 01_introduction_to_algorithms/python/findSmallest.py
@@ -5,7 +5,13 @@ def findSmallest(arr):
 		if arr[i] < smallest:
 			smallest = arr[i]
 			smallest_index = i
-	return smallest_index , smallest
-Smallest, Smallest_index = findSmallest([9456456,45658,45613215,1561232,5156155])
-print(Smallest,Smallest_index)
-	
+	return smallest_index
+Smallest_index = findSmallest([9456456,45658,45613215,1561232,5156155])
+print(Smallest_index)
+def selectionSort(arr):
+	newArr = []
+	for i in range (len(arr)):
+		smallest = findSmallest(arr)
+		newArr.append(arr.pop(smallest))
+	return newArr
+print(selectionSort([9456456,45658,45613215,1561232,5156155]))
